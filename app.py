33597b76b8035d825914d9e4fed22dc07e9fca63
Snikitha <snikitha.karumudi@csedge.org>
2024-07-25 13:15:46 -0700

    added to app.py and added nav bar to all html docs

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, url_for, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/index', methods = ['GET', 'POST'])
+def index():
+    searchData={
+        "nature":["nature.html"],
+        "entertainment": ["ent.html"]
+    }
+
+    if request.method== 'POST':
+        query=request.form['query']
+        if query not in searchData:
+            return "No data found for" + query
+        
+        # need to fix lines 19-22 to go to ent.html when entertainment is typed
+        if query in searchData:
+            return render_template('nature.html', searchData= searchData[query])
+        else:
+          return render_template('ent.html', searchData= searchData[query])  
+        
+
+    return render_template('index.html', url=url_for('index'))
+
+if __name__== "__main__":
+    app.run(host='0.0.0.0', port = 80, debug=True)
